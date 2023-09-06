9ba2c102c7f1744febc6f38284c8e1c94f1cf0ec
Joao <jmc12@illinois.edu>
2023-09-06 16:25:48 -0500

    adding experiment file for albert

---
+++ peanut_segformer_exp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export OMP_NUM_THREADS=5
+export OPENBLAS_NUM_THREADS=5
+export MKL_NUM_THREADS=5
+export VECLIB_MAXIMUM_THREADS=5
+export NUMEXPR_NUM_THREADS=5
+
+export MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet
+
+
+python nav/collect.py -v 1 --dump_location ./data/tmp --exp_name debug --start_ep 0 --end_ep 500  --mapping_strategy neural --evaluation $AGENT_EVALUATION_TYPE --seg_type Segformer -fw 160 -fh 120 --perf_log_name peanut_segformer_part1
+wait
